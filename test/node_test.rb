71fea8d724c66e948c26492aecf9fdd9aec867fe
Boots2291 <buteramv@gmail.com>
2018-10-19 10:37:12 -0400

    Add node structure

--- test/node_test.rb
+++ test/node_test.rb
@@ -13,4 +13,8 @@ class NodeTest < Minitest::Test
   def test_node_knows_its_data
     assert_equal "plop", @node.data
   end
+
+  def test_node_sees_next_node
+    assert_nil @node.next_node
+  end
 end
